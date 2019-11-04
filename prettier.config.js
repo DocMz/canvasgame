a961afde78f162697f81e7b553b6fa52d8f4a483
Moritz <Moritz@DESKTOP-GHLGLD1>
2019-11-04 17:43:08 +0100

    first movable playerObject

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always',
+};
